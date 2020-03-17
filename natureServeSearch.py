9b830d49ca6be04e562b42402e241cfa1329e95f
Anne Maguire <asm0028@srrcmaclt5230.local>
2020-03-17 15:17:11 -0500

    end of first day -- can successfully connect to website but need to work on searching it

--- natureServeSearch.py
+++ natureServeSearch.py
@@ -1,3 +1,13 @@
-import webbrowser
+import requests
+import urllib.request
+import time
+from bs4 import BeautifulSoup
 
-webbrowser.open('http://www.python.org')
+url = 'https://explorer.natureserve.org'
+response = requests.get(url, params={'q': 'sus+scrofa'},)
+print(response) #response 200 means our request to access went through
+##Could add in future: error handling for if the url is wrong?
+
+#soup = BeautifulSoup(response, features="lxml")
+#print(soup)
+#soup.findAll
