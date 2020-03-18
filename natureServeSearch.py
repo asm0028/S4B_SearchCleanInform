d040903b24ff61023eff6a2ab9fe6c8418675c54
Anne Maguire <asm0028@srrcmaclt5230.local>
2020-03-18 15:41:50 -0500

    Have figured out how to put in a query, now need to learn how to scrape the query results page

--- natureServeSearch.py
+++ natureServeSearch.py
@@ -1,13 +1,29 @@
 import requests
-import urllib.request
-import time
+from requests_html import HTMLSession
 from bs4 import BeautifulSoup
+import webbrowser
 
-url = 'https://explorer.natureserve.org'
-response = requests.get(url, params={'q': 'sus+scrofa'},)
-print(response) #response 200 means our request to access went through
+searchbar_url = 'https://explorer.natureserve.org/Search?q='
+query = 'sus%20scrofa'
+
+session = HTMLSession()
+
+page = session.get(searchbar_url+query)
+webbrowser.open(searchbar_url+query)
+#page.html.render()
+#print(page.content)
+
+#results = page.html.search('top-line')
+#print(results)
+#page = requests.get(url)
+#print(page.content)
+#response = requests.get(url, params={'q': 'sus+scrofa'},)
+#print(response) #response 200 means our request to access went through
 ##Could add in future: error handling for if the url is wrong?
 
-#soup = BeautifulSoup(response, features="lxml")
+soup = BeautifulSoup(page.content, 'html.parser')
+results = soup.find(id='resultsSection')
+print(soup.prettify())
+print(results.prettify())
 #print(soup)
 #soup.findAll
