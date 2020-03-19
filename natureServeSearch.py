cbfc4812a0efef82b31fd56397ddbe8708d16b9f
Anne Maguire <asm0028@srrcmaclt5230.local>
2020-03-19 10:16:40 -0500

    Having trouble getting the javascript rendered document to display the species search results. Going to try pypeteer next.

--- natureServeSearch.py
+++ natureServeSearch.py
@@ -9,10 +9,17 @@ query = 'sus%20scrofa'
 session = HTMLSession()
 
 page = session.get(searchbar_url+query)
-webbrowser.open(searchbar_url+query)
-#page.html.render()
+#webbrowser.open(searchbar_url+query)
+rendered = page.html.render()
+#results = page.html.find('#resultsSection')
+#print(results)
+#results.attrs
+#query_search = page.html.search('Sus Scrofa')
+#print(query_search)
 #print(page.content)
 
+species_result = rendered.html.find('a', containing='Taxon')
+print(species_result)
 #results = page.html.search('top-line')
 #print(results)
 #page = requests.get(url)
@@ -21,9 +28,9 @@ webbrowser.open(searchbar_url+query)
 #print(response) #response 200 means our request to access went through
 ##Could add in future: error handling for if the url is wrong?
 
-soup = BeautifulSoup(page.content, 'html.parser')
-results = soup.find(id='resultsSection')
-print(soup.prettify())
-print(results.prettify())
+#soup = BeautifulSoup(page.content, 'html.parser')
+#results = soup.find(id='resultsSection')
+#print(soup.prettify())
+#print(results.prettify())
 #print(soup)
 #soup.findAll
