20d2ad3cce12d65ac9a2c3c969368e9f864046af
Anne Maguire <asm0028@srrcmaclt5230.local>
2020-03-23 15:03:17 -0500

    tried the requests library strategy for bisonSearch but it keeps timing out. The bison api mentions something called opensearch so I will try to interact with that instead

--- bisonSearch.py
+++ bisonSearch.py
@@ -1,11 +1,15 @@
 import requests
+import urllib.request
 from requests_html import HTMLSession
 from bs4 import BeautifulSoup
 import webbrowser
 
-url = "https://bison.usgs.gov/api/search.json?species=Bison bison%type=scientific_name&start=0&count=1"
+url = "https://bison.usgs.gov/api/search.json?species=Bison%20bison%type=scientific_name&start=0&count=1"
 
 #webbrowser.open(url)
 
-page = session.get(url)
-print(page.content)
+request = urllib.request.Request(url)
+html = urllib.request.urlopen(request).read()
+
+soup = BeautifulSoup(html, 'html.parser')
+print(soup.content)
