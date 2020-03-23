5dbcdc033a7985155418caa544962e83b9c14255
Anne Maguire <asm0028@srrcmaclt5230.local>
2020-03-23 14:44:22 -0500

    initial commit for search of bison website, which actually has an API (like gbif) so hopefully this will be easier than natureserve or iucn

---
+++ bisonSearch.py
@@ -0,0 +1,8 @@
+import requests
+from requests_html import HTMLSession
+from bs4 import BeautifulSoup
+import webbrowser
+
+url = "https://bison.usgs.gov/api/search.json?species=Bison bison%type=scientific_name&start=0&count=1"
+
+webbrowser.open(url)
