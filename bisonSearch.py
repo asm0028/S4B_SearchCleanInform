cc476e1fc6901cfd0cc12f03641c8d828f3e982a
Anne Maguire <asm0028@srrcmaclt5230.local>
2020-03-23 15:10:54 -0500

    tried pyops, a package supposedly to interface with OpenSearch through Python. Timed out with initial use and no documentation to troubleshoot so will try something else.

--- bisonSearch.py
+++ bisonSearch.py
@@ -4,12 +4,10 @@ from requests_html import HTMLSession
 from bs4 import BeautifulSoup
 import webbrowser
 
-url = "https://bison.usgs.gov/api/search.json?species=Bison%20bison%type=scientific_name&start=0&count=1"
+url = "https://bison.usgs.gov/api/search.json?species=Bison bison%type=scientific_name&start=0&count=1"
 
 #webbrowser.open(url)
 
-request = urllib.request.Request(url)
-html = urllib.request.urlopen(request).read()
-
-soup = BeautifulSoup(html, 'html.parser')
-print(soup.content)
+import pyops
+client = pyops.Client(description_xml_url=url)
+raw_results = client.search()
