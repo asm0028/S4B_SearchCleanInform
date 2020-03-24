8cfc61aac5fd0c7cc1c80ea48aa9187ca5071ea4
Anne Maguire <asm0028@srrcmaclt5230.local>
2020-03-24 11:00:29 -0500

    converted the output of bisonSearch into a pandas dataframe

--- bisonSearch.py
+++ bisonSearch.py
@@ -2,13 +2,20 @@ import requests
 from bs4 import BeautifulSoup
 import webbrowser
 import json
+import pandas
 
 url = "https://bison.usgs.gov/api/search.json?species=Bison%20bison&type=scientific_name&start=0&count=1"
 
 #webbrowser.open(url) #uncomment to test whether url is valid
-
+matched_species = []
 match = requests.get(url)
 #print(match.content) #uncomment to test whether match is retrieved
 
 match_result = match.json()
-print(match_result)
+match_result["inputName"] = "Bison bison"
+#print(match_result)
+#print(match_result.type)
+
+matched_species.append(match_result)
+result = pandas.DataFrame(matched_species)
+print(result)
