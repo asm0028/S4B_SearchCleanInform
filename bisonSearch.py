953396aace8295290ac0d5951eb535d48dcafc9f
Anne Maguire <asm0028@srrcmaclt5230.local>
2020-03-24 10:35:19 -0500

    Had typo in url that was causing major problems...

--- bisonSearch.py
+++ bisonSearch.py
@@ -4,13 +4,6 @@ from requests_html import HTMLSession
 from bs4 import BeautifulSoup
 import webbrowser
 
-url = "https://bison.usgs.gov/api/search.json?species=Bison bison%type=scientific_name&start=0&count=1"
+url = "https://bison.usgs.gov/api/search.json?species=Bison%20bison&type=scientific_name&start=0&count=1"
 
-#webbrowser.open(url)
-
-import opensearch
-client = Client(url)
-raw_results = client.search()
-
-for result in results:
-    print(result.title, result.link)
+webbrowser.open(url)
