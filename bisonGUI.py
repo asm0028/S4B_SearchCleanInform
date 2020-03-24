3d024f296cea07b9075407c20534d13dcc7cad33
Anne Maguire <asm0028@srrcmaclt5230.local>
2020-03-24 14:47:16 -0500

    Added Go button

--- bisonGUI.py
+++ bisonGUI.py
@@ -12,12 +12,20 @@ greeting_label.grid(row=0, column=0)
 
 species_entry_frame = tk.Frame(master=window)
 species_entry_frame.grid(row=1, column=0)
-#species_entry_frame.pack(fill=tk.X)
+
+species_label = tk.Label(master=species_entry_frame, text = "Please enter the scientific name of your species of interest:")
+species_label.grid(row=0, column=0)
 
 species_entry = tk.Entry(master=species_entry_frame)
 species_entry.insert(0, "Ex: Bison bison")
-species_entry.grid(row=0, column=0)
-#species_entry.pack()
+species_entry.grid(row=0, column=1)
+
+go_button_frame = tk.Frame(master=window, relief=tk.RAISED)
+go_button_frame.grid(row=2, column=0)
+
+go_button = tk.Button(master=go_button_frame, text = "Go!")
+go_button.grid(row=0, column=0)
+
 
 species_name = species_entry.get()
 print(species_name)
