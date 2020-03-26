37520807884129fa649cd1f8f754e443cac02910
ngladstone <nsg0012@auburn.edu>
2020-03-26 12:05:37 -0500

    added output argument and routed it to cwd

--- geobison.py
+++ geobison.py
@@ -14,7 +14,7 @@ import matplotlib.font_manager as fm
 
 #starting geobison block
 
-def geobison(bison_data, map_color='darkgray', map_size=(10,10), marker_color='red', marker_size=10, map_title="[Will insert name of user's species here]"):
+def geobison(bison_data, output=os.getcwd(),  map_color='darkgray', map_size=(10,10), marker_color='red', marker_size=10, map_title="[Will insert name of user's species here]"):
     
     #getting USA data
     url = 'https://eric.clst.org/assets/wiki/uploads/Stuff/gz_2010_us_040_00_5m.json'
@@ -34,4 +34,4 @@ def geobison(bison_data, map_color='darkgray', map_size=(10,10), marker_color='r
     base = CONUS.plot(ax=ax,color=map_color)
     Species_data.plot(ax=base, color=marker_color, marker="*",markersize=marker_size)
     ax.set_title(map_title, fontsize=25)
-    plt.savefig('/Users/NickG/Documents/S4B_Project/GIS_Project_Master_Repo/test.png',bbox_inches='tight')
+    plt.savefig('my_new_map.png', bbox_inches='tight')
