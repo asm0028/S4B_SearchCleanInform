8140f36127eb57a43094c19242b02cb69c60621f
Anne Maguire <asm0028@srrcmaclt5230.local>
2020-03-30 15:14:04 -0500

    added bisonGUI section, added exit button, and started trying to get the return values to work. May need to switch to using tkinter in class mode.

--- bisonAllParts.py
+++ bisonAllParts.py
@@ -2,6 +2,8 @@
 
 import os
 import requests
+import json
+import tkinter as tk
 import numpy as np
 import pandas as pd
 import geopandas as gpd
@@ -10,6 +12,63 @@ from shapely.geometry import Point
 import descartes
 from mpl_toolkits.axes_grid1.anchored_artists import AnchoredSizeBar
 import matplotlib.font_manager as fm
+
+
+
+def build_GUI():
+
+    #event handler functions
+    def handle_go_button(event):
+        species_name = species_entry.get()
+        print(species_name)
+        return species_name
+
+    def close_window():
+        window.destroy()
+
+    #initiate main (master) window
+    window = tk.Tk()
+    window.title("Search-Clean-Inform")
+
+    #place greeting frame in master window and greeting label within that
+    greeting_frame = tk.Frame(master=window, height=25)
+    greeting_frame.grid(row=0, column=0)
+    greeting_label = tk.Label(master=greeting_frame, text="Welcome to Search-Clean-Inform!")
+    greeting_label.grid(row=0, column=0)
+
+    #place species entry frame in master window -- will split into label (query prompt) and entry box
+    species_entry_frame = tk.Frame(master=window)
+    species_entry_frame.grid(row=1, column=0)
+    #place species label (query prompt) within species entry frame
+    species_label = tk.Label(master=species_entry_frame, text = "Please enter the scientific name of your species of interest:")
+    species_label.grid(row=0, column=0)
+    #place species entry box within species entry frame, initialize example text
+    species_entry = tk.Entry(master=species_entry_frame)
+    species_entry.insert(0, "Ex: Bison bison")
+    species_entry.grid(row=0, column=1)
+
+    #place button frame within master window
+    go_button_frame = tk.Frame(master=window, relief=tk.RAISED)
+    go_button_frame.grid(row=2, column=0)
+    #initalize go button and place in button frame
+    go_button = tk.Button(master=go_button_frame, text = "Go!", command = handle_go_button)
+    go_button.grid(row=0, column=0)
+    #initialize exit button and place in button frame
+    exit_button = tk.Button(master=go_button_frame, text = "Exit", command = close_window)
+    exit_button.grid(row=0, column=1)
+
+    #calls for information from GUI and their assignment to return variables
+    species_name = go_button.bind("<Button-1>", command = handle_go_button)
+
+    #keeps window up and running until closed
+    window.mainloop()
+
+    return species_name
+
+species_name = build_GUI()
+#print statements for testing
+print(species_name)
+
 
 """there are two functions housed in this code that perform the following:
 	1.) geobison: provide the geobison with the cleaned bison dataset of
@@ -51,7 +110,7 @@ def geobison(bison_data, output=os.getcwd(),  map_color='darkgray', map_size=(10
 
 #starting geobison_join block
 
-def geobison_count(join_data, output=os.getcwd())
+def geobison_count(join_data, output=os.getcwd()):
 
     #getting USA data
     url = 'https://eric/clst.org/assets/wiki/uploads/Stuff/gz_2010_us_040_00_5m.json'
