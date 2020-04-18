37846ca5742bc925743f51967fbfb03f70536f7b
Anne Maguire <asm0028@srrcmaclt5230.local>
2020-04-18 12:36:28 -0500

    needed for merge

--- bisonAllParts.py
+++ bisonAllParts.py
@@ -174,7 +174,7 @@ species_name_fixed = ('"' + species_name.capitalize() + '"')
 with open('bisonCSV.csv', 'w', newline='') as file:
     wr = csv.writer(file)
     wr.writerow(['scientificName','eventDate','decimalLongitude','decimalLatitude','occurrenceID','catalogNumber','institutionID'])
-        
+
 def fetch_solr(page, rows):
     start = page * rows
     params = {'q' : 'scientificName:' + species_name_fixed, 'rows' : rows, 'start' : start, 'wt' : 'json'}
@@ -183,7 +183,7 @@ def fetch_solr(page, rows):
     match.close()
     return match_result
 
-def result_csv_writer(record):            
+def result_csv_writer(record):
             arr = []
             for keyword in ['scientificName','eventDate','decimalLongitude','decimalLatitude','occurrenceID','catalogNumber','institutionID']:
                 if keyword in (result['response']['docs'][record]):
@@ -193,7 +193,7 @@ def result_csv_writer(record):
             with open ('bisonCSV.csv', 'a', newline='') as file:
                 wr = csv.writer(file)
                 wr.writerow(arr)
-            
+
 result = fetch_solr(0, 1)
 num_found = result['response']['numFound']
 
@@ -201,7 +201,7 @@ if num_found < 1000:
     result = fetch_solr(0, num_found)
     for record in range (0, num_found):
         result_csv_writer(record)
-        
+
 if num_found >= 1000:    
     total_pages = (num_found//1000)
     for page in range (0, total_pages + 1):
@@ -209,12 +209,12 @@ if num_found >= 1000:
         while page != total_pages:
             for record in range (0, 1000):
                 result_csv_writer(record)
-            break   
+            break
             time.sleep(1)
         while page == total_pages:
             for record in range (0, num_found - (total_pages * 1000)):
                 result_csv_writer(record)
-            break   
+            break
 
 #start bisonCleanCSV
 
